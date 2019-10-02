4e5a1a0857b5fc9635f1dd4d95cb91110658938f
prajjwal poudel <iamprazol@gmail.com>
2019-10-02 14:19:55 +0545

    Events, Event Listeners and getElementById implemented

--- app.js
+++ app.js
@@ -6,8 +6,38 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random()* 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    // Random Number Generator
+    dice = Math.floor(Math.random()* 6) + 1;
+
+    //2. Display the result
+    let diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice +'.png';
+
+    //3. Update the round score IF the rolled number was NOT a 1
+    if( dice !== 1){
+        // Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // Next Player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = none;
+    }
+});
